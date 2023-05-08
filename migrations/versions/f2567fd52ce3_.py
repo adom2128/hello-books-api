d0c379c0e9afffa6a526ef0eb3bce503ed5097b6
Alejandra <adom2128@gmail.com>
2023-05-07 20:24:56 -0600

    makes changes to database URI

---
+++ migrations/versions/f2567fd52ce3_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: f2567fd52ce3
+Revises: 
+Create Date: 2023-05-07 20:24:22.155849
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f2567fd52ce3'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
