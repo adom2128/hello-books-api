16713e0fee21fc1629f31ae8a94ee80039f7fb79
Alejandra <adom2128@gmail.com>
2023-05-07 22:18:30 -0600

    updates migration

---
+++ migrations/versions/b7e6abaafedf_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: b7e6abaafedf
+Revises: 9202f064b828
+Create Date: 2023-05-07 22:17:58.330343
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b7e6abaafedf'
+down_revision = '9202f064b828'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
